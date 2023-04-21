54a621a80c084bfa84384a5041d683392042523e
jason-wes <wesse162@umn.edu>
2023-04-21 15:45:40 -0500

    Initial commit

---
+++ part2/connection_queue.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "connection_queue.h"
+
+int connection_queue_init(connection_queue_t *queue) {
+    // TODO Not yet implemented
+    return 0;
+}
+
+int connection_enqueue(connection_queue_t *queue, int connection_fd) {
+    // TODO Not yet implemented
+    return 0;
+}
+
+int connection_dequeue(connection_queue_t *queue) {
+    // TODO Not yet implemented
+    return 0;
+}
+
+int connection_queue_shutdown(connection_queue_t *queue) {
+    // TODO Not yet implemented
+    return 0;
+}
+
+int connection_queue_free(connection_queue_t *queue) {
+    // TODO Not yet implemented
+    return 0;
+}
