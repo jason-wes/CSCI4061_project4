8eedf6f600d878e59df8af41d2e9180c97644216
JStolzb <stolz050@umn.edu>
2023-04-21 18:23:33 -0500

    Part 1 working - needs more testing

--- part1/http.c
+++ part1/http.c
@@ -26,11 +26,153 @@ const char *get_mime_type(const char *file_extension) {
 }
 
 int read_http_request(int fd, char *resource_name) {
-    // TODO Not yet implemented
+    char buf[BUFSIZE];
+    char *token;
+    char *resource_token;
+
+    // make a copy of fd
+    int fd_copy = dup(fd);
+    if (fd_copy == -1) {
+        perror("dup");
+        return -1;
+    }
+
+    FILE *socket_stream = fdopen(fd_copy, "r");
+    if (socket_stream == NULL) {
+        perror("fdopen");
+        close(fd_copy);
+        return -1;
+    }
+
+    // Disable the usual stdio buffering
+    if (setvbuf(socket_stream, NULL, _IONBF, 0) != 0) {
+        perror("setvbuf");
+        fclose(socket_stream);
+        return -1;
+    }
+
+    // Keep consuming lines until we find an empty line
+    while (fgets(buf, BUFSIZE, socket_stream) != NULL) {
+        if (strcmp(buf, "\r\n") == 0) {
+            break;
+        } else {
+            // if its not the last line of the request, can run the same logic on all lines
+            token = strtok(buf, " ");
+
+            // error check strtok
+            if (token == NULL) {
+                perror("strtok");
+                fclose(socket_stream);
+                return -1;
+            }
+
+            // At the beginning of the request, after GET, next strtok will give the resource name
+            if (strcmp(token, "GET") == 0) {
+                resource_token = strtok(NULL, " ");
+
+                // error check strtok
+                if (resource_token == NULL) {
+                    perror("strtok");
+                    fclose(socket_stream);
+                    return -1;
+                }
+                
+                // concatentate strings and error check
+                if (strcat(resource_name, resource_token) == NULL) {
+                    perror("strcat");
+                    fclose(socket_stream);
+                    return -1;
+                }
+            }
+        }
+    }
+
+    if (fclose(socket_stream) != 0) {
+        perror("fclose");
+        return -1;
+    }
+
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
+    char http_response[BUFSIZE];
+    char buf[BUFSIZE];
+    if (access(resource_path, F_OK) == 0) {
+        // file exists
+        int file = open(resource_path, O_RDONLY);
+        if(file == -1) {
+            perror("open");
+            return -1;
+        }
+
+        // get file size
+        struct stat file_info;
+        if(fstat(file, &file_info) == -1) {
+            perror("fstat");
+            return -1;
+        }
+        int content_length = file_info.st_size;
+
+        int bytes_written;
+        int bytes_read;
+
+        // get file type
+        const char *extension = strrchr(resource_path, '.');
+        if (extension == NULL) {
+            perror("strrchr");
+            close(file);
+            return -1;
+        }
+        const char *content_type = get_mime_type(extension);
+        if (content_type == NULL) {
+            printf("error getting content type for http repsonse");
+            close(file);
+            return -1;
+        }
+
+        // constructs http_response header
+        sprintf(http_response, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n", content_type, content_length);
+
+        // write http_response header
+        bytes_written = write(fd, http_response, strlen(http_response));
+
+        if (bytes_written < 0) {
+            perror("write");
+            close(file);
+            return -1;
+        }
+
+        // read file data and write to TCP fd
+        while((bytes_read = read(file, buf, BUFSIZE)) > 0) {
+            if(bytes_read == -1) {
+                perror("read");
+                close(file);
+                return -1;
+            }
+            int total_written = 0;
+            while(total_written < bytes_read) {
+                bytes_written = write(fd, buf, bytes_read - total_written);
+                if(bytes_written < 0) {
+                    perror("write");
+                    close(file);
+                    return -1;
+                }
+                total_written += bytes_written;
+            }
+        }
+
+
+        // write in chunks
+    } else {
+        // file doesnt exist
+        strcpy(http_response, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+        
+        write(fd, &http_response, strlen(http_response));
+    }
+
+
+
     // TODO Not yet implemented
     return 0;
 }
--- part1/http_server.c
+++ part1/http_server.c
@@ -26,8 +26,96 @@ int main(int argc, char **argv) {
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    // const char *serve_dir = argv[1];
-    // const char *port = argv[2];
+    const char *serve_dir = argv[1];
+    const char *port = argv[2];
+
+    // Catch SIGINT so we can clean up properly
+    struct sigaction sigact;
+    sigact.sa_handler = handle_sigint;
+    sigfillset(&sigact.sa_mask);
+    sigact.sa_flags = 0; // Note the lack of SA_RESTART
+    if (sigaction(SIGINT, &sigact, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
+
+    // Set up hints - we'll take either IPv4 or IPv6, TCP socket type
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // We'll be acting as a server
+    struct addrinfo *server;
+
+    // Set up address info for socket() and connect()
+    int ret_val = getaddrinfo(NULL, port, &hints, &server);
+    if (ret_val != 0) {
+        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(ret_val));
+        return 1;
+    }
+    // Initialize socket file descriptor
+    int sock_fd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+    if (sock_fd == -1) {
+        perror("socket");
+        freeaddrinfo(server);
+        return 1;
+    }
+    // Bind socket to receive at a specific port
+    if (bind(sock_fd, server->ai_addr, server->ai_addrlen) == -1) {
+        perror("bind");
+        freeaddrinfo(server);
+        close(sock_fd);
+        return 1;
+    }
+    freeaddrinfo(server);
+    // Designate socket as a server socket
+    if (listen(sock_fd, LISTEN_QUEUE_LEN) == -1) {
+        perror("listen");
+        close(sock_fd);
+        return 1;
+    }
+
+    char resource_name[BUFSIZE];
+    while (keep_going != 0) {
+        // Wait to receive a connection request from client
+        // Don't both saving client address information
+        int client_fd = accept(sock_fd, NULL, NULL);
+        if (client_fd == -1) {
+            if (errno != EINTR) {
+                perror("accept");
+                // free (resource_name);
+                close(sock_fd);
+                return 1;
+            } else {
+                break;
+            }
+        }
+        
+        
+        strcpy(resource_name, serve_dir);
+        if(read_http_request(client_fd, resource_name) == -1) {
+            perror("read_http");
+            // free(resource_name);
+            close(client_fd);
+            close(sock_fd);
+            return 1;
+        } // serve_dir/resource_name
+
+        if(write_http_response(client_fd, resource_name) == -1) {
+            perror("write_http");
+            // free(resource_name);
+            close(client_fd);
+            close(sock_fd);
+            return 1;
+        }
+    }
+    // free(resource_name);
+
+    // Don't forget cleanup - reached even if we had SIGINT
+    if (close(sock_fd) == -1) {
+        perror("close");
+        return 1;
+    }
 
     // TODO Complete the rest of this function
     return 0;
